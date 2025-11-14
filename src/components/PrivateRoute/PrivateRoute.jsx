f1d3d3e7baece76459c88e983cdce8b1c50f0890
matteda ramesh <mattedaramesh27@gmail.com>
2025-11-14 19:48:04 +0530

    changes in the private router

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
-import Cookies from "js-cookie";
+import axios from "axios";
 
 function PrivateRoute({ children }) {
-  const token = Cookies.get("token");
-  console.log("token", token)
-  const isAuthenticated = !!token;
+  const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const apiUrl = import.meta.env.VITE_API_BASE_URL;
+
+  const autheticateUser = async () => {
+    // Ask backend if user is authenticated
+    await axios
+      .get(`${apiUrl}/api/v1/user/auth`, { withCredentials: true })
+      .then((res) => {
+        if (res.data.status) setIsAuthenticated(res.data.status);
+      })
+      .catch(() => setIsAuthenticated(false))
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => autheticateUser, []);
+  console.log("isAuthenticated", isAuthenticated);
+
+  if (loading) return <div>Loading...</div>;
+
   return isAuthenticated ? children : <Navigate to="/login" replace />;
 }
 
